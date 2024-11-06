1444752b5f767dd43f97b0a2285d9846f4f1188a
исп-34 <-34@LAB21-04>
2024-11-06 12:37:06 +0300

    Добавьте файлы проекта.

---
+++ PR6/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics.Metrics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TriardClass;
+
+namespace PR6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Информация пока не заполненно");
+        }
+
+        private void btCalculation_Click(object sender, RoutedEventArgs e)
+        {
+            Triard triardOne = new Triard();
+            Triard triardTwo = new Triard();
+            bool boolOneFirstPositiveNumber = int.TryParse(tbOneTriardFirstNumber.Text, out int oneFirstPositiveNumber);
+            bool boolTwoFirstPositiveNumber = int.TryParse(tbTwoTriardFirstNumber.Text, out int twoFirstPositiveNumber);
+            bool boolThreeFirstPositiveNumber = int.TryParse(tbThreeTriardFirstNumber.Text, out int threeFirstPositiveNumber);
+            bool boolOneSecondPositiveNumber = int.TryParse(tbOneTriardSecondNumber.Text, out int oneSecondPositiveNumber);
+            bool boolTwoSecondPositiveNumber = int.TryParse(tbTwoTriardSecondNumber.Text, out int twoSecondPositiveNumber);
+            bool boolThreeSecondPositiveNumber = int.TryParse(tbThreeTriardSecondNumber.Text, out int threeSecondPositiveNumber);
+            if (boolOneFirstPositiveNumber == true || boolTwoFirstPositiveNumber == true || boolThreeFirstPositiveNumber == true || boolOneSecondPositiveNumber == true || boolTwoSecondPositiveNumber == true || boolThreeSecondPositiveNumber == true)
+            {
+                triardOne.SetParams(oneFirstPositiveNumber, twoFirstPositiveNumber, threeFirstPositiveNumber);
+                triardTwo.SetParams(oneSecondPositiveNumber, twoSecondPositiveNumber, threeSecondPositiveNumber);
+                if (triardOne == triardTwo) MessageBox.Show("Триады равны");
+                else MessageBox.Show("Триады не равны");
+            }
+            else MessageBox.Show("Туда сюда");
+        }
+    }
+}
---
+++ PR6/TriardClass.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TriardClass
+{
+    class Triard
+    {
+        public int onePositiveNumber;
+        public int twoPositiveNumber;
+        public int threePositiveNumber;
+        public int OnePositiveNumber
+        {
+            get
+            {
+                return onePositiveNumber;
+            }
+            set
+            {
+                if (value > 0) onePositiveNumber = value;
+                else MessageBox.Show("В первом числе меньше нуля");
+
+            }
+        }
+        public int TwoPositiveNumber
+        {
+            get
+            {
+                return twoPositiveNumber;
+            }
+            set
+            {
+                if (value > 0) twoPositiveNumber = value;
+                else MessageBox.Show("Во втором числе меньше нуля");
+            }
+        }
+        public int ThreePositiveNumber
+        {
+            get
+            {
+                return threePositiveNumber;
+            }
+            set
+            {
+                if (value > 0) threePositiveNumber = value;
+                else MessageBox.Show("В третьем числе меньше нуля");
+            }
+        }
+        public int SumNumber()
+        {
+            return OnePositiveNumber + TwoPositiveNumber + ThreePositiveNumber;
+        }
+        public void SetParams(int oneSetParams, int twoSetParams, int threeSetParams)
+        {
+            OnePositiveNumber = oneSetParams;
+            TwoPositiveNumber = twoSetParams;
+            ThreePositiveNumber = threeSetParams;
+        }
+        public void SetParams(int oneSetParams, int twoSetParams)
+        {
+            OnePositiveNumber = oneSetParams;
+            TwoPositiveNumber = twoSetParams;
+            ThreePositiveNumber = oneSetParams + twoSetParams;
+        }
+        public void SetParams(int oneSetParams)
+        {
+            OnePositiveNumber = oneSetParams;
+            TwoPositiveNumber = oneSetParams;
+            ThreePositiveNumber = oneSetParams;
+        }
+        public static bool operator ==(Triard c1, Triard c2)
+        {
+            bool result;
+            if (c1.onePositiveNumber == c2.onePositiveNumber && c1.twoPositiveNumber == c2.twoPositiveNumber && c1.threePositiveNumber == c2.threePositiveNumber) result = true;
+            else result = false;
+            return result;
+        }
+        public static bool operator !=(Triard c1, Triard c2) 
+        {
+            return c1.onePositiveNumber != c2.onePositiveNumber && c1.twoPositiveNumber != c2.twoPositiveNumber && c1.threePositiveNumber != c2.threePositiveNumber;
+        }
+    }
+}
